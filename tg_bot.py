97998063f955f40ee3f8e724beedb541f7b4f391
basher-balance <b_438@mail.ru>
2022-04-20 17:08:41 +0600

    first commit

--- tg_bot.py
+++ tg_bot.py
@@ -5,6 +5,8 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
 
 
 
@@ -23,11 +25,45 @@ def start(update: Update, context: CallbackContext):
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+# При написание команды /caps последующие символы выйдут в верхнем регистре
+def caps(update: Update, context: CallbackContext):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+def inline_caps(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+#При команде через / если команда неизвеста, выйдет сообщение
+def unknown(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
-
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
 updater.start_polling()
+
